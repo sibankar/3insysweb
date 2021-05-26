118af6a1071bae6efc691cf56e3a55dcf1053dcb
sibankar <Sibankardey1.sd@gmail.com>
2021-05-26 14:42:59 +0530

    last work without pay

--- Content/js/main.js
+++ Content/js/main.js
@@ -1,4 +1,4 @@
-$(document).ready(function() {
-    var divHeight = $('.navbar').outerHeight(); 
-    $('.navigation-bar').css('height', divHeight+'px');
-});
+//$(document).ready(function() {
+ //   var divHeight = $('.navbar').outerHeight(); 
+ //   $('.navigation-bar').css('height', divHeight+'px');
+//});
